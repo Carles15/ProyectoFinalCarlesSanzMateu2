d7786fa519d532f19467e24309e853345e37ff2c
unknown <Magiic15@gmail.com>
2015-05-07 19:26:26 +0200

    My first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectofinal2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyectofinal2App
+ */
+angular.module('proyectofinal2App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectofinal2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proyectofinal2App
+ */
+angular.module('proyectofinal2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
